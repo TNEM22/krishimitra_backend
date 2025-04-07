7fcdc6138d60805a6f6dd33f47eb397dd370c480
tnem <tanmay.e.mahale@gmail.com>
2025-04-07 23:39:26 +0530

    added-get-users

--- routers/auth.py
+++ routers/auth.py
@@ -1,13 +1,20 @@
 from fastapi import APIRouter, HTTPException
 from models.user import SignupRequest
-from services.voximplant_service import create_voximplant_user
+from services.voximplant_service import create_voximplant_user, get_online_users
 
 router = APIRouter()
 
+
 @router.post('/signup')
 async def signup(request: SignupRequest):
-  success = await create_voximplant_user(request.username, request.display_name, request.password)
-  # print(success)
-  if not success:
-    raise HTTPException(status_code=400, detail="Fail to create user.")
-  return {"message": "User created successfully"}
+    success = await create_voximplant_user(request.username, request.display_name, request.password)
+    # print(success)
+    if not success:
+        raise HTTPException(status_code=400, detail="Fail to create user.")
+    return {"message": "User created successfully"}
+
+
+@router.get('/users/online')
+async def users():
+    res = await get_online_users()
+    return {"data": res}
--- services/voximplant_service.py
+++ services/voximplant_service.py
@@ -7,12 +7,13 @@ load_dotenv()
 VOX_ACCOUNT_ID = os.getenv("VOXIMPLANT_ACCOUNT_ID")
 VOX_API_KEY = os.getenv("VOXIMPLANT_API_KEY")
 VOX_APP_ID = os.getenv("VOXIMPLANT_APP_ID")
-VOX_API_URL = "https://api.voximplant.com/platform_api/AddUser/"
+VOX_API_URL = "https://api.voximplant.com/platform_api/"
+
 
 async def create_voximplant_user(username: str, display_name: str, password: str) -> bool:
     async with httpx.AsyncClient() as client:
         response = await client.post(
-            VOX_API_URL,
+            VOX_API_URL + 'AddUser/',
             data={
                 "account_id": VOX_ACCOUNT_ID,
                 "api_key": VOX_API_KEY,
@@ -24,3 +25,18 @@ async def create_voximplant_user(username: str, display_name: str, password: str
         )
         # print(response.json())
         return response.status_code == 200 and response.json().get("result")
+
+
+async def get_online_users():
+    async with httpx.AsyncClient() as client:
+        response = await client.get(VOX_API_URL + 'GetUsers/', params={
+            "account_id": VOX_ACCOUNT_ID,
+            "api_key": VOX_API_KEY,
+            "application_id": VOX_APP_ID,
+        })
+        print(response.json())
+        if response.status_code != 200:
+            return []
+
+        data = response.json()
+        return data.get("result", [])
